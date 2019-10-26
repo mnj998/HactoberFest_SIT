9102584b58a82cd3c6af4f49138b47df9e5311d7
mnj998 <57027110+mnj998@users.noreply.github.com>
2019-10-25 23:16:54 -0700

    used pow()
    because pow() is more efficient

--- HactoberFest_Programs/exp.c
+++ HactoberFest_Programs/exp.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-int exp(int a,int b){
-	if(b==0)
-		return 1;
-	else
-		return a*exp(a,b-1);
+#include<math.h>
+int expo(int a,int b){
+	return pow(a,b);
 }
+
 int main(){
 	int num1,num2,res=0;
 	printf("\nEnter 2 numbers : ");
 	scanf("%d%d",&num1,&num2);
-	res=exp(num1,num2);
+	res=expo(num1,num2);
 	printf("\nExponent of %d^%d is : %d\n",num1,num2,res);
 	return 0;
 }
